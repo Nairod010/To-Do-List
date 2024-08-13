3dc8cb7bd2ddbe6dde3255fec09ab5e990bf441e
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-08-13 23:12:12 +0300

    ignore setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ git add .gitignore
 git commit -m "Ignore setup.sh"
 
 # Push changes to the repository
-echo "Pushing changes to the repository..."
-git push origin main
+#echo "Pushing changes to the repository..."
+#git push origin main
 
 echo "Setup complete"
 
