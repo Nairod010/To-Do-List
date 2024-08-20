9f20c20922b4df36412cf0bd74bcbc9a65e13df6
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-08-20 13:46:35 +0300

    rebuilding

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './styles.css';
-import { build } from './models';
-import { display } from './display';
-
-const newTodo = build.createTodo("title")
-const newProject = build.createProject("project")
-
-newProject.addToDo(newTodo)
-display.fillNavigation(newProject)
-display.addProject()
-
-//logic for adding to the project needed here
-display.formButtons()
-
-console.log(newProject)
