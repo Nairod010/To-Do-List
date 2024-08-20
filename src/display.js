63c2d24ad329e00b06abb649d8d1b47066ae10c1
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-08-20 22:42:57 +0300

    display wip

--- src/display.js
+++ src/display.js
@@ -8,6 +8,7 @@ class Elements{
         this.dialCloseButton = document.querySelector("dialog .close")
         this.input = document.querySelector("input")
         this.navigation = document.querySelector("nav")
+        this.projectPreview = document.querySelector(".project-view")
     }
     
     createProject(name){
@@ -15,8 +16,32 @@ class Elements{
         newButton.classList.add(`nav-project`)
 
         newButton.textContent = `${name}`
-
+        
         this.navigation.appendChild(newButton)
+
+        return newButton
+    }
+
+    createProjectPreview(project){
+        this.projectPreview.textContent = ""
+        const projectTitle = document.createElement("div")
+        projectTitle.classList.add("title")
+        projectTitle.textContent = project.title
+        
+        const projectTasksContainer = document.createElement("div")
+        projectTasksContainer.classList.add("tasks-container")
+        
+        const addTask = document.createElement("button")
+        addTask.classList.add("add-task")
+        addTask.textContent = "Add"
+        
+
+        projectTasksContainer.appendChild(addTask)
+
+        this.projectPreview.appendChild(projectTitle) 
+        this.projectPreview.appendChild(projectTasksContainer)
+
+        return addTask
     }
 }
 
@@ -34,9 +59,15 @@ export const display = {
         dom.dialAddButton.addEventListener("click", (e) => {
             e.preventDefault();
             const project = new Project(dom.input.value)
-            projectsList.push(project)
-            dom.createProject(dom.input.value)
-            console.log(projectsList)
+            const newButton = dom.createProject(project.title)
+
+            newButton.addEventListener("click", () => {
+                if(dom.projectPreview.textContent !== "" || dom.projectPreview.textContent !== project.title){
+                   dom.createProjectPreview(project)
+                }
+            })
+            projectsList.push()
+
             dom.dial.close()
             dom.input.value = ""
         })
