5c1c41343baf544f71fd2f90f072cb30a2b917bd
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-08-23 11:16:05 +0300

    done with adding projects

--- src/dispaly.js
+++ src/dispaly.js
@@ -16,18 +16,36 @@ export const display = function() {
     inputProjectTitle.value = ""
     
     const confirmButton = helper("button","confirm-title-button", "confrim")
+    confirmButton.addEventListener("click", () => {
+        const newProject = app.createProject()
+        newProject.setProjectTitle(inputProjectTitle.value)
+
+        inputProjectTitle.remove() 
+        confirmButton.remove()
+        projectRemover.remove()
+
+        projectList.push(newProject)
+        
+        inputProjectTitle.value = ""
+        console.log(projectList)
+    })
+
+    const projectList = app.projectList()
+
 
     const projectAdder = helper("button", "project-adder", "New Project")
     projectAdder.addEventListener("click", () => {
         navbar.appendChild(inputProjectTitle)
         navbar.appendChild(confirmButton)
+        navbar.appendChild(projectRemover)
     })
 
-    const projectRemover = helper("button", "project-remover", "Remove")
+    const projectRemover = helper("button", "project-cancel", "cancel")
     projectRemover.addEventListener("click", () => {
-
+        inputProjectTitle.remove() 
+        confirmButton.remove()
+        projectRemover.remove()
     })
 
     navbar.appendChild(projectAdder)
-    navbar.appendChild(projectRemover)
 }
